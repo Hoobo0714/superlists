886e7006e9fdbf51150abf427a18a32c2b727802
Hoobo0714 <Hoobo0714@foxmail.com>
2019-01-16 13:55:55 +0800

    second commit

--- function_tests.py
+++ function_tests.py
@@ -1,5 +1,21 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
-assert'Django' in browser.title
+class NerVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        #她去看了应用的首页
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('ToDo',self.browser.title)
+        self.fail('finish the test')
+
+if __name__ == '__main__':
+    unittest.main()
